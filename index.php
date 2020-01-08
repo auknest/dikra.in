193edf9a41993f9926a38e0854aaf784e844f6eb
shogale-10 <52746595+shogale-10@users.noreply.github.com>
2020-01-08 11:16:58 +0530

    save

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/*
+$fp = fsockopen("smtp.googlemail.com", 587, $errno, $errstr, 10);
+if (!$fp) {
+    echo "$errstr ($errno)<br />\n";
+} else {
+    echo "opened";
+}
+die;*/
+// Version
+define('VERSION', '2.3.0.2');
+
+// Configuration
+if (is_file('config.php')) {
+	require_once('config.php');
+}
+
+// Install
+if (!defined('DIR_APPLICATION')) {
+	header('Location: install/index.php');
+	exit;
+}
+
+// VirtualQMOD
+require_once('./vqmod/vqmod.php');
+VQMod::bootup();
+
+// VQMODDED Startup
+require_once(VQMod::modCheck(DIR_SYSTEM . 'startup.php'));
+
+start('catalog');
